8b6433a6b9c17caf482e96dc748a42b3507bd43b
elliottwu <elliottwu@users.noreply.github.com>
2021-08-20 15:26:38 +0100

    changed links to data and models

--- pretrained/download_pretrained_syn_vase.sh
+++ pretrained/download_pretrained_syn_vase.sh
@@ -1,2 +1,2 @@
 echo "----------------------- downloading pretrained model on met vases -----------------------"
-curl -o met_vase_pretrained.zip "https://www.robots.ox.ac.uk/~szwu/storage/sorderender/met_vase_pretrained.zip" && unzip met_vase_pretrained.zip
+curl -o met_vase_pretrained.zip "https://www.robots.ox.ac.uk/~vgg/research/sorderender/data/met_vase_pretrained.zip" && unzip met_vase_pretrained.zip
