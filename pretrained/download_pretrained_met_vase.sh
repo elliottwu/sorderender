fc655bf66b66876a8bfa589ad16a55ab0860204d
elliottwu <elliottwu@users.noreply.github.com>
2023-02-01 13:51:28 +0000

    fixed typo in download scripts

--- pretrained/download_pretrained_met_vase.sh
+++ pretrained/download_pretrained_met_vase.sh
@@ -1,2 +1,2 @@
-echo "----------------------- downloading pretrained model on synthetic vases -----------------------"
-curl -o syn_vase_pretrained.zip "https://www.robots.ox.ac.uk/~vgg/research/sorderender/data/syn_vase_pretrained.zip" && unzip syn_vase_pretrained.zip
+echo "----------------------- downloading pretrained model on met vases -----------------------"
+curl -o met_vase_pretrained.zip "https://www.robots.ox.ac.uk/~vgg/research/sorderender/data/met_vase_pretrained.zip" && unzip met_vase_pretrained.zip
